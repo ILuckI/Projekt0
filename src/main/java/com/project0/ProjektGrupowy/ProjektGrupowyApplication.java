81b4c8072893f04530e19d0055711ae9cada5fa2
Unknown <wodzynski.mateusz@gmail.com>
2018-06-30 12:27:15 +0200

    Czysty szablon

---
+++ src/main/java/com/project0/ProjektGrupowy/ProjektGrupowyApplication.java
@@ -0,0 +1,12 @@
+package com.project0.ProjektGrupowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGrupowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektGrupowyApplication.class, args);
+	}
+}
