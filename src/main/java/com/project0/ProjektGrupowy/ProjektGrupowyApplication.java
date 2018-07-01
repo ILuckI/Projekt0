7cd6d78e3137acb0f6c294ec1c7ef1d8b90240f8
Unknown <wodzynski.mateusz@gmail.com>
2018-07-01 12:28:52 +0200

    Pom

--- src/main/java/com/project0/ProjektGrupowy/ProjektGrupowyApplication.java
+++ src/main/java/com/project0/ProjektGrupowy/ProjektGrupowyApplication.java
@@ -1,12 +1,19 @@
 package com.project0.ProjektGrupowy;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjektGrupowyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjektGrupowyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektGrupowyApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
 }
