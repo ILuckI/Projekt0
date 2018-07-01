55f81f529944e9e1244266551aec680f7d51b97e
Unknown <wodzynski.mateusz@gmail.com>
2018-07-01 14:53:56 +0200

    Cennik

---
+++ src/main/java/com/project0/ProjektGrupowy/repository/PriceRepository.java
@@ -0,0 +1,7 @@
+package com.project0.ProjektGrupowy.repository;
+
+import com.project0.ProjektGrupowy.Entities.Price;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PriceRepository extends CrudRepository<Price, Long> {
+}
---
+++ src/main/java/com/project0/ProjektGrupowy/service/PriceService.java
@@ -0,0 +1,29 @@
+package com.project0.ProjektGrupowy.service;
+
+import com.project0.ProjektGrupowy.Entities.Car;
+import com.project0.ProjektGrupowy.Entities.Price;
+import com.project0.ProjektGrupowy.dto.CarDto;
+import com.project0.ProjektGrupowy.dto.PriceDto;
+import com.project0.ProjektGrupowy.repository.PriceRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class PriceService {
+
+    @Autowired
+    private PriceRepository priceRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<PriceDto> getAllPrices(){
+        Iterable<Price> prices = priceRepository.findAll();
+        return StreamSupport.stream(prices.spliterator(), false)
+                .map(price -> modelMapper.map(price, PriceDto.class))
+                .collect(Collectors.toList());
+    }
+}
