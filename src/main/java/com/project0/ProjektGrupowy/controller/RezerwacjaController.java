b99a67c49cc54f1c70b4572064654352c53a972e
Unknown <wodzynski.mateusz@gmail.com>
2018-07-07 11:31:29 +0200

    Rezerwacja

--- src/main/java/com/project0/ProjektGrupowy/controller/RezerwacjaController.java
+++ src/main/java/com/project0/ProjektGrupowy/controller/RezerwacjaController.java
@@ -1,15 +1,27 @@
 package com.project0.ProjektGrupowy.controller;
 
+import com.project0.ProjektGrupowy.dto.CarDto;
+import com.project0.ProjektGrupowy.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by Luck
  */
 @Controller
 public class RezerwacjaController {
+
+    @Autowired
+    private CarService carService;
+
     @RequestMapping("/rezerwacja")
-    public String home() {
-        return "pages/rezerwacja";
+    public ModelAndView home() {
+        List<CarDto> allCars = carService.getAllCars();
+        return new ModelAndView("pages/rezerwacja", "cars", allCars);
     }
+
 }
