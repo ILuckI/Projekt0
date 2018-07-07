9835eb36a8cf4f710de55d8704eb1b19af6a4392
Unknown <wodzynski.mateusz@gmail.com>
2018-07-07 14:09:17 +0200

    Kontrollery

---
+++ src/main/java/com/project0/ProjektGrupowy/controller/CarInaccessibleController.java
@@ -0,0 +1,29 @@
+package com.project0.ProjektGrupowy.controller;
+
+import com.project0.ProjektGrupowy.dto.CarDto;
+import com.project0.ProjektGrupowy.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Luck
+ */
+@Controller
+public class CarInaccessibleController {
+
+    @Autowired
+    private CarService carService;
+
+    @RequestMapping("/carInaccessible")
+    public ModelAndView home() {
+        List<CarDto> allCars = carService.getAllCars();
+        allCars.sort(Comparator.comparing(CarDto::getCarName));
+        return new ModelAndView("pages/carInaccessible", "cars", allCars);
+    }
+
+}
